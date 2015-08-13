beef9075372bdeea0ef9005277f21b6c05888533
Jannik B <technofreak91@web.de>
2015-08-13 14:00:19 +0200

    Found another one
    Go away.

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,5 @@
 <hr>
 <table id="footertable">
-<<<<<<< HEAD
 	<tr>
 		<td class="footercontent">
 			<ul>
@@ -18,25 +17,6 @@
 			</ul>
 		</td>
 	</tr>
-=======
-    <tr>
-        <td class="footercontent">
-            <ul>
-                <li><strong>More</strong></li>
-                <li><a href="?m=poststatic&id=1">About</a></li>
-                <li><a href="?m=poststatic&id=2">Imprint</a></li>
-            </ul>
-        </td>
-        <td class="footercontent">
-            <ul>
-                <li><strong>Staff</strong></li>
-                <li><a href="?m=user&id=1">Jannik "gobbo" Balkau</a></li>
-                <li><a href="?m=user&id=2">Miah</a></li>
-                <li><a href="?m=user&id=3">Your mum</a></li>
-            </ul>
-        </td>
-    </tr>
->>>>>>> origin/unstable
 </table>
 <hr>
 Powered by Nuntii<br>
